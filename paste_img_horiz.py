95987a5b3077b78fe0cb1eeaec57e9a4b69fced7
WizardOfOzzie <wizardofozzie@users.noreply.github.com>
2016-03-16 00:25:38 +1100

    Update paste_img_horiz.py
    Appex and tweaked for loop (no need for specifying number of images)

--- paste_img_horiz.py
+++ paste_img_horiz.py
@@ -1,16 +1,27 @@
 # coding: utf-8
-# https://gist.github.com/b6c87590a06081175d8f
-import Image, photos, dialogs, sys
+## Paste chosen/shared images horizontally into a new image, then save and show new image
+
+import Image, photos, dialogs, sys, appex
+
 
 images = []
-number_of_images = int(dialogs.input_alert("# of images?"))
 
-assert 1 <= number_of_images <=36
+if appex.is_running_extension():
+    images = appex.get_images()
+else:
+    while True:
+        im = photos.pick_image()
+        if im is None:
+            break
+        images.append(im)
 
-for i in range(number_of_images):
-    images.append(photos.pick_image())
+
+number_of_images = len(images)
+assert 1 <= number_of_images <= 50
+
 
 widths, heights = zip(*(j.size for j in images))
+
 
 total_width = sum(widths)
 max_height = max(heights)
@@ -21,11 +32,12 @@ x_offset = 0
 for im in images:
     new_image.paste(im, (x_offset, 0))
     x_offset += im.size[0]
-    
+
+
 try:
     photos.save_image(new_image)
     dialogs.hud_alert("Saved!")
     new_image.show()
-except Exception:
+except:
     dialogs.hud_alert("could not save file!".title(), "error")
     sys.exit(0)
