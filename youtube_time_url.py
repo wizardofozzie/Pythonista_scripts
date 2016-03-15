040cab1b67c84fd0bae4736d251336cf53a81f96
WizardOfOzzie <wizardofozzie@users.noreply.github.com>
2016-03-16 09:26:51 +1100

    Update and rename appex_youtube_time_url.py to youtube_time_url.py

---
+++ youtube_time_url.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+## Takes a youtube URL and time code and copies time-coded URL to clipboard
+import appex, requests, clipboard, console, dialogs, collections, sys
+
+# Thanks to @cclauss
+# from https://github.com/cclauss/Ten-lines-or-less/blob/master/form_dialog_from_fields_dict.py
+    
+def form_dialog_from_fields_dict(title, fields_dict):
+    return dialogs.form_dialog(title, [{'title': k, 'type': v} for k, v in fields_dict.iteritems()])
+
+
+my_fields_dict1 = collections.OrderedDict((
+    ('URL',      'url'),
+    ('Time',     'text')))
+
+my_fields_dict2 = collections.OrderedDict((
+    ('Time',     'text'), ))
+
+if appex.is_running_extension():
+    url = appex.get_url()
+    d = form_dialog_from_fields_dict("Enter timestamp as mm:ss", my_fields_dict2)
+    ts = d['Time']
+else:
+    d = form_dialog_from_fields_dict("Enter YouTube URL and time (mm:ss)", my_fields_dict1)
+    url = d['URL']
+    ts = d['Time']
+
+
+def main():
+    assert any([x in url for x in ("youtube", "youtu.be")]), "{0} is not a YouTube URL!".format(url)
+    assert ":" in ts, "timestamp must be written as (hh:)mm:ss"
+    
+    if ts.count(":") == 1:
+        mins, secs = map(int, ts.split(":"))
+        hrs = 0
+    elif ts.count(":") == 2:
+        hrs, mins, secs = map(int, ts.split(":"))
+    else:
+        sys.stderr.write("Bad timestamp (too many ':')")
+        sys.exit(0)
+    
+    seconds = 3600*hrs + 60*mins + secs
+    newurl = "{url}?t={seconds}".format(url=url, seconds=3600*hrs + 60*mins + secs)
+    
+    clipboard.set(newurl)
+    dialogs.hud_alert("{0} copied to clipboard".format(newurl))
+    return newurl 
+
+main()
