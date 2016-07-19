b94f980fad8af1fc350575bc157c11b46bbe4c52
WizardOfOzzie <wizardofozzie@users.noreply.github.com>
2016-07-19 14:52:08 +1000

    Update youtube_time_url.py
    Fixed for Python 3

--- youtube_time_url.py
+++ youtube_time_url.py
@@ -6,22 +6,25 @@ import appex, requests, clipboard, console, dialogs, collections, sys
 # from https://github.com/cclauss/Ten-lines-or-less/blob/master/form_dialog_from_fields_dict.py
     
 def form_dialog_from_fields_dict(title, fields_dict):
-    return dialogs.form_dialog(title, [{'title': k, 'type': v} for k, v in fields_dict.iteritems()])
+    return dialogs.form_dialog(title, [{'title': k, 'type': v} for k, v in fields_dict.items()])
 
 
 my_fields_dict1 = collections.OrderedDict((
-    ('URL',      'url'),
-    ('Time',     'text')))
+    ('URL',      'url'), ('Time',     'text')
+    ))
 
 my_fields_dict2 = collections.OrderedDict((
-    ('Time',     'text'), ))
+                         ('Time',     'text'),
+    ))
 
 if appex.is_running_extension():
     url = appex.get_url()
     d = form_dialog_from_fields_dict("Enter timestamp as mm:ss", my_fields_dict2)
+    if d is None: sys.exit()
     ts = d['Time']
 else:
     d = form_dialog_from_fields_dict("Enter YouTube URL and time (mm:ss)", my_fields_dict1)
+    if d is None: sys.exit()
     url = d['URL']
     ts = d['Time']
 
@@ -39,8 +42,8 @@ def main():
         sys.stderr.write("Bad timestamp (too many ':')")
         sys.exit(0)
     
-    seconds = 3600*hrs + 60*mins + secs
-    newurl = "{url}?t={seconds}".format(url=url, seconds=3600*hrs + 60*mins + secs)
+    seconds = hrs*(60**2) + mins*(60**1) + secs*(60**0)
+    newurl = "{url}?t={seconds}".format(url=url, seconds=seconds)
     
     clipboard.set(newurl)
     dialogs.hud_alert("{0} copied to clipboard".format(newurl))
