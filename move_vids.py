d437590bfd7cd715bdddf17fd45577ff974d29f2
Simcity <djsim4242@gmail.com>
2016-08-30 07:09:16 +0000

    move_vids.py works

--- move_vids.py
+++ move_vids.py
@@ -8,22 +8,26 @@ VIDEO_EXTENSIONS = [
     ".mp4", ".m4a"
     ]
 
-def get_vid_names(dir="~/Documents", fullpath=True):
+
+def get_vid_names(dir="~/Documents", fullpath=False):
     assert path.isdir(path.expanduser(dir))
     filez = [f for f in os.listdir(path.expanduser(dir)) if path.isfile(f)]
     ret = [x for x in filez if path.splitext(x)[-1] in VIDEO_EXTENSIONS]
     return ret if not fullpath else map(lambda x: path.join(path.expanduser(dir), x)[8:], ret)
 
 
-# TODO: fix exception
 def move_vids(fromdir='~/Documents', todir='~/Documents/VIDEO'):
-    vidlist = get_vid_names(fromdir, True)
+    vidlist = get_vid_names(fromdir)
     for f in vidlist:
         try:
             os.rename(
-                path.join(path.expanduser(fromdir), f), 
-                path.expanduser(todir)
+                path.join(path.expanduser(fromdir), f)[8:], 
+                path.join(path.expanduser(todir), f)[8:]
                 )
+            print("==> {0}/{1}".format(todir, f))
         except:
             print("Could not move file:\t {fn}".format(fn=f))
 
+
+if __name__ == "__main__":
+    move_vids()
